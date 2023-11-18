d75d1cf20ce295a795d00a3e30c53da3751c8a6e
BIGGY00 <bowbigzaza2014@gmail.com>
2023-11-18 10:18:06 +0700

    fix bug

--- app/models/db.js
+++ app/models/db.js
@@ -1,23 +1,19 @@
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config");
-
-require("dotenv").config();
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-
+// const dbConfig = require("../config/db.config");
 // const connection = mysql.createConnection({
 //     host: dbConfig.HOST,
 //     user: dbConfig.USER,
 //     password: dbConfig.PASSWORD,
-//     database: dbConfig.DB,
-//     PORT: dbConfig.PORT
+//     database: dbConfig.DB
 // });
 
-connection.connect(error => {
-    if (error) {
-        console.log("MySQL connection: " + error);
-    } else {
-        console.log("Successfully connected to the database.");
-    }
-});
+//For PlanetScale
+require('dotenv').config()
+const connection = mysql.createConnection(process.env.DATABASE_URL)
 
+
+connection.connect((error)=>{
+    if(error) console.log("MYSQL connection: " + error);
+    else console.log("Successfully connected to the database");
+});
 module.exports = connection;
