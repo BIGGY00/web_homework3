36cc12cdeaba883945b7c9484f963bc029597f94
BIGGY00 <bowbigzaza2014@gmail.com>
2023-11-18 10:05:26 +0700

    fix bug

--- app/controllers/file.controller.js
+++ app/controllers/file.controller.js
@@ -1,37 +1,32 @@
-const uploadFile = require("../middleware/upload.js");
-const upload = async (req, res) => {
-  try {
-    await uploadFile(req, res);
-    if (req.file == undefined) {
-      return res.status(400).send({
-        message: "Please Upload a file!.",
-      });
+const uploadFile = require("../middleware/upload");
+const uploadController = async (req, res)=>{
+    try {
+        console.log("file controller")
+        await uploadFile(req, res);
+        if(req.file == undefined){
+            return res.status(400).send({
+                message: "Not Found the upload file."
+            });
+        }
+        res.status(200).send({
+            message: "Upload file successfully: " + req.file.filename,
+            uploadFileName: req.file.filename
+        });
+    } catch (error) {
+        res.status(500).send({ 
+            message: "Could not upload the file: " + error
+        });
     }
-    res.status(200).send({
-      //miss should be req
-      message: "Upload the file successfully: " + req.file.filename,
-      uploadFileName: req.file.filename,
-    });
-  } catch (err) {
-    res.status(500).send({
-      message: "Could not upload the file: " + err,
-    });
-  }
 };
 
-const download = (req, res) => {
-  const fileName = req.params.name;
-  const directoryPath = __basedir + "/uploads/";
-  res.download(directoryPath + fileName, fileName, (err) => {
-    if (err) {
-      res.status(500).send({
-        message: "Could not download the file." + err,
-      });
-    }
-  });
+const displayAvatar = (req, res)=>{
+    const filename = req.params.name;
+    const directoryPath = __basedir + "/assets/";
+    console.log("a:" + directoryPath)
+    res.download(directoryPath + filename, filename, (err)=>{
+        if(err){
+            res.status(500).send({ message: "Could not display the file. " + err});
+        }
+    });
 };
-
-module.exports = {
-  upload,
-  download,
-};
+module.exports = { uploadController, displayAvatar};
